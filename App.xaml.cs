b7ac8792fceb564a340db83da26cd226ca789d81
Luanaferreira22 <luannaferreira080@gmail.com>
2025-09-14 10:53:00 -0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using VacinaApp.Data;
+
+namespace VacinaApp
+{
+    public partial class App : Application
+    {
+        public PacienteRepository PacienteRepo { get; private set; }
+        public UsuarioRepository UsuarioRepo { get; private set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            var databasePath = Path.Combine(FileSystem.AppDataDirectory, "vacinaapp.db3");
+            PacienteRepo = new PacienteRepository(databasePath);
+            UsuarioRepo = new UsuarioRepository(databasePath);
+
+            MainPage = new AppShell();
+        }
+
+        protected override async void OnStart()
+        {
+            base.OnStart();
+
+            await PacienteRepo.Init();
+            await UsuarioRepo.Init();
+        }
+    }
+}
